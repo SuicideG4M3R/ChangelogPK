4fd679d0872768a8f4d25368ea5967a657ddefda
SuicideG4M3R <suicideg4m3r@gmail.com>
2024-05-10 23:03:37 +0200

    prisma/database

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-const express = require('express')
+import express from 'express'
+import path from 'path'
+
 const app = express();
-const path = require('path')
 
 app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-    // res.json({message: 'Hello Express'})
-    res.sendFile(path.resolve("public/pages/index.html"));
+    res.json({message: 'Hello Express'})
 });
 app.listen(3002, () => {
     console.log('Server started on port 3002')
